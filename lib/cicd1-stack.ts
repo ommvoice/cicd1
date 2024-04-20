066ed167b6561cd08bca4dee619cb89620f871ea
Umer Farooq <umerfarooq@Umers-Mac-mini.lan>
2024-04-20 19:59:52 +0100

    initial

---
+++ lib/cicd1-stack.ts
@@ -0,0 +1,25 @@
+import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Cicd1Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('ommvoice/cicd1', 'main'), //Remember to change 
+        commands: ['npm ci', 
+                   'npm run build', 
+                   'npx cdk synth']
+      })
+    });
+
+    const testingStage = pipeline.addStage(new PipelineStage(this, "test", {
+      env: { account: "664876795774", region: "eu-west-3" }
+    }));
+  }
+}
---
+++ test/cicd1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cicd1 from '../lib/cicd1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cicd1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cicd1.Cicd1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
