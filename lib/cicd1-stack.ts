46b7e1a6b3a878a2d6fee95e736a268170d2ca6d
Umer Farooq <umerfarooq@Umers-Mac-mini.lan>
2024-04-20 20:38:44 +0100

    5test

--- lib/cicd1-stack.ts
+++ lib/cicd1-stack.ts
@@ -1,5 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { PipelineStage } from './PipelineStage';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -11,15 +16,21 @@ export class Cicd1Stack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('ommvoice/cicd1', 'master'), //Remember to change 
-        commands: ['npm ci', 
-                   'npm run build', 
-                   'npx cdk synth']
-      })
+        input: CodePipelineSource.gitHub('ommvoice/cicd1', 'master'), //Remember to change
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
     });
 
-    const testingStage = pipeline.addStage(new PipelineStage(this, "test", {
-      env: { account: "664876795774", region: "eu-west-3" }
-    }));
+    const testingStage = pipeline.addStage(
+      new PipelineStage(this, 'test', {
+        env: { account: '664876795774', region: 'eu-west-3' },
+      })
+    );
+
+    testingStage.addPre(
+      new CodeBuildStep('unit-tests', {
+        commands: ['npm ci', 'npm run test'],
+      })
+    );
   }
 }
