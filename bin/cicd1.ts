f34eb40fa8e6f7a31eaf1b9204b01a27d54bd511
Umer Farooq <umerfarooq@Umers-Mac-mini.lan>
2024-04-20 20:26:03 +0100

    4test

--- bin/cicd1.ts
+++ bin/cicd1.ts
@@ -5,6 +5,10 @@ import { Cicd1Stack } from '../lib/cicd1-stack';
 
 const app = new cdk.App();
 new Cicd1Stack(app, 'Cicd1Stack', {
+  env: {
+    account: "664876795774",    // replace with your aws account ID
+    region: "eu-west-3",       // replace with your preferred region
+  },
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
